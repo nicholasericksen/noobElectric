03db2fbe58cff07ee596c62b1fc615ed2f42c90d
Nicholas Ericksen <nericksen.student@manhattan.edu>
2017-09-10 15:13:10 -0400

    Upgrade to webpack 3

--- app/frontend/public/js/index.jsx
+++ app/frontend/public/js/index.jsx
@@ -1,5 +1,10 @@
-import App from './App';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import BrowserRouter from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('reactEntry'));
+import App from './App';
+
+ReactDOM.render(
+    <App />, document.getElementById('reactEntry')
+);
--- app/frontend/webpack.config.js
+++ app/frontend/webpack.config.js
@@ -1,14 +1,26 @@
 var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
   entry: [
     "./public/js/index.jsx"
   ],
   output: {
-    path: __dirname + '/public/static',
+    path: path.join(__dirname, '/public/static'),
     filename: "bundle.js"
   },
   module: {
-    loaders: [
+    rules: [
+        {
+            test: /\.less$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+        },
       {
         test: /\.js?$/,
         loader: 'babel-loader',
@@ -25,14 +37,16 @@ module.exports = {
         },
         exclude: /node_modules/
     },
-    {
-        test: /\.less$/,
-        loader: "style!css!less"
-    }
+    // { test: /\.css$/, loader: "style-loader!css-loader", exclude: /node_modules/ },
+    // {
+    //     test: /\.less$/,
+    //     loader: "style-loader!less-loader!less",
+    //     exclude: /node_modules/
+    // }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
 },
   plugins: [
   ]
