8c8367c0ab6402013b790b9b24d54a0e5f882d6c
Nicholas Ericksen <nericksen.student@manhattan.edu>
2017-10-01 16:48:20 -0400

    Rename old conventioned data

--- app/data/rename_files.py
+++ app/data/rename_files.py
@@ -2,19 +2,19 @@ import os
 import glob
 from shutil import copy
 
-directories = glob.glob('./american-ash-*')
+directories = glob.glob('./matte-*')
 
 for directory in directories:
         for filename in os.listdir(directory):
                 print filename
                 path = os.path.join('./',directory)
-                if (filename == '0.png'):
+                if (filename == '90.png'):
                         print 'Renamed H for ', directory
                         copy(os.path.join(path,filename),  os.path.join(path, 'H.png'))
                 if (filename == '45.png'):
                         print 'Renamed P for ', directory
                         copy(os.path.join(path,filename),  os.path.join(path, 'P.png'))
-                if (filename == '90.png'):
+                if (filename == '0.png'):
                         print 'Renamed V for ', directory
                         copy(os.path.join(path,filename),  os.path.join(path, 'V.png'))
                 if (filename == '135.png'):
